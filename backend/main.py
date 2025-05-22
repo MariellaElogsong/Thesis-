ed233b6e64d33555742cdc5e0fae93aada586c52
Mariella Elogsong <mariellaelogsong02@gmail.com>
2025-05-22 15:05:04 +0800

    May 22

--- backend/main.py
+++ backend/main.py
@@ -7,5 +7,9 @@ def home():
     message = "This message is generated from Python!"
     return render_template('index.html', message=message)
 
+@app.route('/result')
+def result():
+    return render_template('result.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
